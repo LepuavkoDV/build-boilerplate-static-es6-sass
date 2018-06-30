46d612be50af054c4de3b8ded29f6bbd69618be0
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-07-01 02:36:55 +0300

    included style.css/index.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         loader: 'babel-loader'
       },
       {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(woff(2)?|ttf|eot|svg|png|jpg|jpeg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader',
         query: {
           name: '[path][name].[ext]',
