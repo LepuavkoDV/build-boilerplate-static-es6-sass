d97a6e4dd4f4977035b76ff5848467c3d05af91e
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-07-04 19:37:16 +0300

    customised start script

--- start.sh
+++ start.sh
@@ -1,1 +1,17 @@
-code . && npm start
+#!/bin/bash
+GREEN='\033[0;32m'
+NC='\033[0m'
+file=./package-lock.json
+if [ -e "$file" ]; then
+  printf "Starting ${GREEN}VS Code${NC}\n"
+  code .
+  printf "Starting ${GREEN}WDS${NC}\n"
+  npm start
+else 
+  printf "Running ${GREEN}npm${NC} install\n"
+  npm install
+  printf "Starting ${GREEN}VS Code${NC}\n"
+  code .
+  printf "Starting ${GREEN}WDS${NC}\n"
+  npm start
+fi
