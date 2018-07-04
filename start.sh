73150867b4d161ead852b85ed06c67a6d1cc216f
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-07-05 01:11:38 +0300

    colors in start script changed to more light

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
+BLUE='\033[1;34m'
 NC='\033[0m'
 file=./package-lock.json
 project=${PWD##*/}
