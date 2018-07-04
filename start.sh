07969ccc1428aaa611284831d623b758803cc2a2
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-07-04 20:03:57 +0300

    aditional customisation for start script

--- start.sh
+++ start.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
 NC='\033[0m'
 file=./package-lock.json
+project=${PWD##*/}
 if [ -e "$file" ]; then
-  printf "Starting ${GREEN}VS Code${NC}\n"
+  printf "\n${GREEN}***${NC} Starting ${BLUE}${project}${NC} ${GREEN}***${NC}\n\n"
+  printf "Starting ${YELLOW}VSCode${NC}\n"
   code .
-  printf "Starting ${GREEN}WDS${NC}\n"
+  printf "Starting ${YELLOW}WDS${NC}\n"
   npm start
-else 
-  printf "Running ${GREEN}npm${NC} install\n"
+else
+  printf "\n${RED}***${NC} Fresh boilerplate clone detected ${RED}***${NC}\n\n"
+  printf "Running ${YELLOW}npm${NC} install\n\n"
   npm install
-  printf "Starting ${GREEN}VS Code${NC}\n"
+  printf "\n${GREEN}***${NC} Installation complete ${GREEN}***${NC}\n"
+  printf "${GREEN}***${NC} Starting ${BLUE}${project}${NC} ${GREEN}***${NC}\n\n"
+  printf "Starting ${YELLOW}VSCode${NC}\n"
   code .
-  printf "Starting ${GREEN}WDS${NC}\n"
+  printf "Starting ${YELLOW}WDS${NC}\n"
   npm start
 fi
